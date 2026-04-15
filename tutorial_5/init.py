5fecac566b3fc3d7d9451ca064748db19caa9563
Paul Sardin <paulsardin123@gmail.com>
2026-04-15 09:09:16 +0200

    [tutorial_5] Keep init.py to problem setup, move plotTraj to plot.py

--- tutorial_5/init.py
+++ tutorial_5/init.py
@@ -1,6 +1,5 @@
 import numpy as np
 from pinocchio import SE3, neutral
-from pyhpp.core import RandomShortcut, SimpleTimeParameterization
 from pyhpp.manipulation import (
     Device,
     Graph,
@@ -139,47 +138,3 @@ if p1 is None:
     print("Failed to plan p1 after 50 attempts. Try running the script again.")
 else:
     print(f"Path p1 planned (q_init -> qpg), length: {p1.length():.3f}")
-
-    # Optimize the geometric path before time parameterization
-    shortcut = RandomShortcut(problem)
-    p1_opt = shortcut.optimize(p1)
-    print(f"Optimized path length: {p1_opt.length():.3f} (was {p1.length():.3f})")
-
-    # Time parameterization with low acceleration to visualize the difference
-    stp = SimpleTimeParameterization(problem)
-    stp.order = 2
-    stp.safety = 0.95
-    stp.maxAcceleration = 0.5
-    p1_stp = stp.optimize(p1_opt)
-    print(f"STP duration: {p1_stp.length():.3f} s (maxAcceleration=0.5)")
-
-    In a separate file `plot.py`, we could add the following function that plots the trajectory, velocity and accelerations. This implies installing `matplotlib` in the docker image
-
-import matplotlib.pyplot as plt
-# Plot a trajectory with respect to times
-#
-#  \param p pyhpp.core.Path instance
-#  \param rmin, rmax: range of the path to be plot. For example passing rmin=0, rmax=6 will
-#                     plot the 6 first coordinates of the path,
-#  \param dt: time step,
-#  \param order: 0 for configuration, 1 for configuration and velocity, 2 for configuration
-#                velocity and acceleration
-def plotTraj(p, rmin, rmax, dt = 0.01, order = 2):
-    times = dt * np.arange(int(p.length()/dt))
-    fig = plt.figure(layout = "constrained")
-    gs = fig.add_gridspec(order+1,1)
-    values = np.array([p.eval(t)[0] for t in times])
-    ax0 = fig.add_subplot(gs[0,0])
-    if order >= 1:
-        velocities = np.array([p.derivative(t,1) for t in times])
-        ax1 = fig.add_subplot(gs[1,0])
-    if order >= 2:
-        accelerations = np.array([p.derivative(t,2) for t in times])
-        ax2 = fig.add_subplot(gs[2,0])
-    for i in range(rmin, rmax):
-        ax0.plot(times, values[:,i], label=f"q{i}")
-        if order >= 1:
-            ax1.plot(times, velocities[:,i], label=f"q{i}")
-        if order >= 2:
-            ax2.plot(times, accelerations[:,i], label=f"q{i}")
-    fig.show()
